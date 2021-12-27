05673b52ef2661f142b790c6be646a43eb998063
edudi <edudiaz7979@gmail.com>
2021-12-27 03:09:30 -0600

    gtysdrdhgtfsdhtgjdghnf

--- src/main/java/gnamj/unkwars/UnkWars.java
+++ src/main/java/gnamj/unkwars/UnkWars.java
@@ -4,6 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class UnkWars extends JavaPlugin {
 
+    //HIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIHIH
+
+
+
+
+    //BUTY BUT FART, STUPID STUPID
     @Override
     public void onEnable() {
         // Plugin startup logic
